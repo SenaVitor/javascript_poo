ccb1d8370f15701c7ef02354358e80729eb067a8
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-07-19 22:24:01 -0300

    create function criarCurso

--- Admin.js
+++ Admin.js
@@ -4,8 +4,13 @@ class Admin extends User{
     constructor(nome, email, nascimento, role, ativo = true){
         super(nome, email, nascimento, role, ativo);
     }
+
+    criarCurso(curso, vagas){
+        return `Curso de ${curso} criado com ${vagas} vagas`;
+    }
 }
 
 const novoAdmin = new Admin('Vitor', 'v@gmail.com', '2022-07-19');
 console.log(novoAdmin);
 console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCurso('JavaScript', 30));
