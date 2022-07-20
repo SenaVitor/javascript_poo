4a36f55f2f4e7b62f8c91d6088ec7b1d92984a57
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-07-20 19:29:08 -0300

    export the Admin class

--- Admin.js
+++ Admin.js
@@ -1,6 +1,6 @@
 import User from './User.js';
 
-class Admin extends User{
+export default class Admin extends User{
     constructor(nome, email, nascimento, role = 'admin', ativo = true){
         super(nome, email, nascimento, role, ativo);
     }
@@ -9,8 +9,3 @@ class Admin extends User{
         return `Curso de ${curso} criado com ${vagas} vagas`;
     }
 }
-
-const novoAdmin = new Admin('Vitor', 'v@gmail.com', '2022-07-19');
-console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
-console.log(novoAdmin.criarCurso('JavaScript', 30));
