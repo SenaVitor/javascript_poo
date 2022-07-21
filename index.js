642d308fe1aacd1789d9b6af77358c042b4e8458
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-07-20 21:56:06 -0300

    change the admin name

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const novoUsuario = new User('Vitor', "v@gmail.com", '2022-07-20');
 console.log(novoUsuario.exibirInfos());
 
 const novoAdmin = new User('Rodrigo', "r@gmail.com", '2021-04-10');
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.nome);
+novoAdmin.nome = '';
+console.log(novoAdmin.nome);
